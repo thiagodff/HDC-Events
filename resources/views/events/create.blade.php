e97301f59d7c1f6a1aa5c488fd35ee87f43fe13a
Thiago Fernandes Dornelles <thiago.fdornelles@gmail.com>
2021-01-08 16:07:54 -0300

    feat: create event

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -3,5 +3,41 @@
 @section('title', 'Criar Evento')
 
 @section('content')
-  <h1>Criar um evento</h1>
+  <div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie o seu evento</h1>
+
+    <form action="/events" method="POST">
+      @csrf
+
+      <div class="form-group">
+        <label for="title">Evento</label>
+        <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento" />
+      </div>
+
+      <div class="form-group">
+        <label for="title">Cidade</label>
+        <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento" />
+      </div>
+
+      <div class="form-group">
+        <label for="title">O evento é privado?</label>
+        <select name="private" id="private" class="form-select">
+          <option value="0">Não</option>
+          <option value="1">Sim</option>
+        </select>
+      </div>
+
+      <div class="form-group">
+        <label for="title">Descrição</label>
+        <textarea
+          class="form-control"
+          id="description"
+          name="description"
+          placeholder="O que vai acontecer no evento"
+        ></textarea>
+      </div>
+
+      <input type="submit" value="Criar Evento" class="btn btn-primary">
+    </form>
+  </div>
 @endsection
