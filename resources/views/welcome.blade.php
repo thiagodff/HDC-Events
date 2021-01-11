e7206f4c38158da595346631a34e3b3dd360ea9d
Thiago Fernandes Dornelles <thiago.fdornelles@gmail.com>
2021-01-11 08:56:25 -0300

    chore: search events

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,18 @@
 @section('content')
 <div id="search-container" class="col-md-12">
   <h1>Busque um evento</h1>
-  <form action="">
+  <form action="/" method="GET">
     <input type="text" id="search" name="search" class="form-control" placeholder="procurar" />
   </form>
 </div>
 
 <div id="events-container" class="col-md-12">
+  @if($search)
+  <h2>Buscando por: {{ $search }}</h2>
+  @else
   <h2>Próximos Eventos</h2>
   <p class="subtitle">Veja os eventos dos próximos dias</p>
+  @endif
 
   <div id="cards-container" class="row">
     @foreach($events as $event)
@@ -28,7 +32,10 @@
     </div>
     @endforeach
 
-    @if(count($events) == 0)
+    @if($search)
+    <p>Não foi possível encontrar nenhum evento com {{ $search }}</p>
+    <a href="/">Ver todos</a>
+    @elseif(count($events) == 0)
     <p>Não há eventos disponíveis</p>
     @endif
   </div>
